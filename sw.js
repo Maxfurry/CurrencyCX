3811ee728dc8427ca7f33d003abe4551c292c4e1
Maxfurry <32283153+Maxfurry@users.noreply.github.com>
2018-06-30 21:55:34 +0100

    Fixing service worker issues

--- sw.js
+++ sw.js
@@ -6,73 +6,49 @@
 **/
 
 // register cache name
-var version = 'v1::';
-var CacheResources = [
-	'/',
-	'js/converter.js',
-	'js/indexed_db.js'
+let cacheName = 'currnx-v1';
+let CacheResources = [
+	'https://maxfurry.github.io/CurrencyCX/',
+	'https://maxfurry.github.io/CurrencyCX/converter.js',
+	'https://maxfurry.github.io/CurrencyCX/index.html',
+'https://maxfurry.github.io/CurrencyCX/js/indexed_db.js'
 ];
 
-// Wait before activation until cache
-self.addEventListener('install', function(event){
-	event.waitUntil(
-		caches.open(version + 'fundamentals').then(function(cache){
-			return cache.addAll(CacheResources);
-		})
-	);
+
+self.addEventListener('install', function(e) {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(contentToCache);
+    })
+  );
 });
 
-// processing request offline first
-self.addEventListener('fetch', function(event) {
-if (event.request.method !='GET') {
-	consle.log('ignore fetch', event.request.method, event.request.url);
-	return;
-}
-event.respondWith(
-	caches.match(event.request).then(function(cached) {
-var networked = fetch(event.request)
-.then(fetchedFromNetwork, unableToResolve)
-.catch(unableToResolve);
-return cached || networked;
 
-function fetchedFromNetwork(response) {
-	var cacheCopy = response.clone();
-	caches.open(version + 'pages')
-	.then(function add(cache) {
-		cache.put(event.request, cacheCopy);
-	})
-	.then(function(){
-			console.log('fetch response stored in cache', event.request.url);
-	});
-	return response;
-}
-
-function unableToResolve() {
-	console.log('fetch request failed in both cache and network');
-	return new Response('<h1> Service Unavailable</h1>', {
-		status: 503,
-		statusText: 'Service Unavailable',
-		headers: new Headers({
-		'content-Type': 'text/html'
-		})
-	});
-}
-})
-);
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then(function(response) {
+                return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
 
-self.addEventListener('activate', function(event) {
-	event.waitUntil(caches.keys().then(function(keys) {
-			return Promise.all(
-				keys.filter(function(key) {
-			return !key.startsWith(version);
-		})
-		.map(function(key) {
-			return caches.delete(key);
-		})
-	);
-}).then(function() {
-	console.log('service worker is activated completely');
-})
-);
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+          return Promise.all(keyList.map(function(key) {
+        if(cacheName.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
 });
