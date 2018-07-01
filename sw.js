3374a39f100381c856f5e41c2dd91aa61db4f6b0
Maxfurry <32283153+Maxfurry@users.noreply.github.com>
2018-07-01 22:32:10 +0100

    CDN Jquery

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ var CacheResources = [
 	'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css',
 	'https://maxfurry.github.io/CurrencyCX/js/converter.js',
 	'https://maxfurry.github.io/CurrencyCX/js/indexed_db.js',
-	'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+	'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js',
 	'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js',
 	'https://free.currencyconverterapi.com/api/v5/currencies',
 	'https://cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js'
